b26c0c1a230da2bbcab880bfae8eec95d96ddfde
Anni <anni.o.vaahtoranta@gmail.com>
2024-04-02 12:56:08 +0300

    2.10 (osa 2b valmis)

--- osa2/2b_puhelinluettelo/src/App.jsx
+++ osa2/2b_puhelinluettelo/src/App.jsx
@@ -1,59 +1,19 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filterText, setFilterText] = useState('')
-
-  const addNumber = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
-    const personObject = {
-      name: newName,
-      number: newNumber
-    }
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)}
-    else{
-      setPersons(persons.concat(personObject))
-      setNewName('')
-    }
-
-  }
-  const handleNameChange = (event) => {
-    console.log('name changed', event.target.value)
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    console.log('number changed', event.target.value)
-    setNewNumber(event.target.value)
-  }
-
-  const handleFilterChange = (event) => {
-    console.log('filter changed', event.target.value)
-    setFilterText(event.target.value)
-  }
-  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filterText.toLowerCase()))
-
-  const Numbers = () => personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
+const FilterForm = ({handleFilterChange}) =>{
+  return(
+    <form>
         <div>
           Filter shown with:
           <input onChange={handleFilterChange}/>
         </div>
       </form>
-      <h2>Add a new number</h2>
-      <form onSubmit={addNumber}>
+  )
+}
+
+const AddForm = ({addNumber, newName, handleNameChange, newNumber, handleNumberChange}) => {
+  return(
+    <form onSubmit={addNumber}>
         <div>
           name: 
           <input 
@@ -72,8 +32,62 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <Numbers/>
+  )
+}
+
+const Numbers = ({personsToShow}) => personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterText, setFilterText] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)}
+    else{
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+
+  }
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilterText(event.target.value)
+  }
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filterText.toLowerCase()))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <FilterForm handleFilterChange={handleFilterChange}/>
+      <h3>Add a new number</h3>
+      <AddForm 
+        addNumber={addNumber} 
+        newName={newName} 
+        handleNameChange={handleNameChange} 
+        newNumber={newNumber} 
+        handleNumberChange={handleNumberChange} 
+      />
+      <h3>Numbers</h3>
+      <Numbers personsToShow={personsToShow}/>
     </div>
   )
 
